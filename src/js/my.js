29f62a8964272892f04da38d66ef29418791a127
Alex112 <oleksandr.mais@gmail.com>
2019-06-28 13:22:21 +0300

    add slider

--- src/js/my.js
+++ src/js/my.js
@@ -1,14 +1,3 @@
 $(document).ready(function () {
-    $('.slider-box').slick({dots: true, arrows: false, autoplay: true});
-    $("ul.tabs-caption").on("click", "li:not(.active)", function () {
-        $(this)
-            .addClass("active")
-            .siblings()
-            .removeClass("active")
-            .closest("div.tabs")
-            .find("div.tabs-content")
-            .removeClass("active")
-            .eq($(this).index())
-            .addClass("active");
-    });
+    $('.slider__list').slick({dots: true, arrows: true, autoplay: false});
 });
