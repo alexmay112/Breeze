91be305f99eb74ceeb7ea1edd3d11ee56bcf4370
Alex112 <oleksandr.mais@gmail.com>
2019-06-28 19:19:53 +0300

    fix slider's arrows

--- src/js/my.js
+++ src/js/my.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-    $('.slider__list').slick({dots: true, arrows: true, autoplay: false});
+    $('.slider__list').slick({
+        dots: true,
+        arrows: true,
+        autoplay: false,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></' +
+                'button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i><' +
+                '/button>'
+    });
 });
