5f17de917fd98b260d6648aae3a2f6cd57849568
Alex112 <oleksandr.mais@gmail.com>
2019-06-30 22:29:17 +0300

    fix code another

--- src/js/breeze.js
+++ src/js/breeze.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.slider__list').slick({
         dots: true,
         arrows: true,
-        autoplay: false,
+        autoplay: true,
         prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></' +
                 'button>',
         nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i><' +
